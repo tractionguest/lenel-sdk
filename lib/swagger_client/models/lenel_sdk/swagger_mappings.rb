8a57b85353473430eef20536f79d454541c9bcfd
nickrobertson <nbr1983@gmail.com>
2025-01-21 10:12:44 -0700

    add mappings

---
+++ lib/swagger_client/models/lenel_sdk/swagger_mappings.rb
@@ -0,0 +1,11 @@
+module LenelSDK
+  Instance = SwaggerClient::LenelSdk::Instance
+  ApiError = SwaggerClient::ApiError
+  InstanceDefinition = SwaggerClient::InstanceDefinition
+  Credentials = SwaggerClient::Credentials
+  UsersApi = SwaggerClient::UsersApi
+  InstancesApi = SwaggerClient::InstancesApi
+  Configuration = SwaggerClient::Configuration
+  AuthenticationApi = SwaggerClient::AuthenticationApi
+  ApiClient = SwaggerClient::ApiClient
+end
